945c1ba42691e359c05f2fde052a0431ebabb91a
AlexF99 <alexandreopf@gmail.com>
2022-08-08 15:12:22 -0300

    nova organizacao de classes

--- src/main/java/quebra/ListaOfertadas.java
+++ src/main/java/quebra/ListaOfertadas.java
@@ -1,35 +1,8 @@
 package quebra;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaOfertadas {
-    class Ofertada extends Disciplina {
-        int periodoIdeal;
-        int versaoDescSituacao;
-        int numHoras;
-
-        public Ofertada(String codCurso,
-                String nomeCurso,
-                String nomeDisciplina,
-                String codDisciplina,
-                int cargaHoraria,
-                int numVersao,
-                int tipo_disciplina,
-                int periodoIdeal,
-                int versaoDescSituacao,
-                int numHoras) {
-            super(codCurso, nomeCurso, nomeDisciplina, codDisciplina, cargaHoraria, numVersao, tipo_disciplina);
-            this.periodoIdeal = periodoIdeal;
-            this.versaoDescSituacao = versaoDescSituacao;
-            this.numHoras = numHoras;
-        }
-
-        // getters
-        public int getperiodoIdeal(){return this.periodoIdeal;}
-        public int getversaoDescSituacao(){return this.versaoDescSituacao;}
-        public int getnumHoras(){return this.numHoras;}
-        
-        //setters
-        public void setPeriodoIdeal(int periodoIdeal){this.periodoIdeal = periodoIdeal;}
-        public void setVersaoDescSituacao(int versaoDescSituacao){this.versaoDescSituacao = versaoDescSituacao;}
-        public void setNumHoras(int numHoras){this.numHoras = numHoras;}
-    }
+    List<Ofertada> listaOfertadas = new ArrayList<>();
 }
---
+++ src/main/java/quebra/Ofertada.java
@@ -0,0 +1,49 @@
+package quebra;
+
+public class Ofertada extends Disciplina {
+    int periodoIdeal;
+    int versaoDescSituacao;
+    int numHoras;
+
+    public Ofertada(String codCurso,
+            String nomeCurso,
+            String nomeDisciplina,
+            String codDisciplina,
+            int cargaHoraria,
+            int numVersao,
+            int tipoDisciplina,
+            int periodoIdeal,
+            int versaoDescSituacao,
+            int numHoras) {
+        super(codCurso, nomeCurso, nomeDisciplina, codDisciplina, cargaHoraria, numVersao, tipoDisciplina);
+        this.periodoIdeal = periodoIdeal;
+        this.versaoDescSituacao = versaoDescSituacao;
+        this.numHoras = numHoras;
+    }
+
+    // getters
+    public int getperiodoIdeal() {
+        return this.periodoIdeal;
+    }
+
+    public int getversaoDescSituacao() {
+        return this.versaoDescSituacao;
+    }
+
+    public int getnumHoras() {
+        return this.numHoras;
+    }
+
+    // setters
+    public void setPeriodoIdeal(int periodoIdeal) {
+        this.periodoIdeal = periodoIdeal;
+    }
+
+    public void setVersaoDescSituacao(int versaoDescSituacao) {
+        this.versaoDescSituacao = versaoDescSituacao;
+    }
+
+    public void setNumHoras(int numHoras) {
+        this.numHoras = numHoras;
+    }
+}
