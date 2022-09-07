08c245c3b9d2ae8b3384a04aa60e23aad775aaae
AlexF99 <alexandreopf@gmail.com>
2022-09-07 12:17:08 -0300

    adicionando lista de ffaltante para a quebra de barreira

--- src/main/java/quebra/FrontEnd.java
+++ src/main/java/quebra/FrontEnd.java
@@ -11,20 +11,21 @@ import javax.swing.table.DefaultTableModel;
 public class FrontEnd extends JFrame implements ActionListener {
 
     private DefaultTableModel modelCursadas = new DefaultTableModel();
+    private DefaultTableModel modelFaltantes = new DefaultTableModel();
 
     ListaCursadas listaCursadas = ListaCursadas.getInstance();
     ListaOfertadas listaOfertadas = ListaOfertadas.getInstance();
 
     public FrontEnd() {
-        // JFrame f = new JFrame();
-
         JTable cursadasView = cursadas();
         JLabel aprovacao = aprovacaoUltimo();
+        JTable faltantes = faltantes();
 
         this.getContentPane().add(cursadasView);
         this.getContentPane().add(aprovacao);
+        this.getContentPane().add(faltantes);
 
-        this.setSize(600, 450);
+        this.setSize(1000, 1000);
     }
 
     private JTable cursadas() {
@@ -46,7 +47,7 @@ public class FrontEnd extends JFrame implements ActionListener {
             }
         }
 
-        cursadasTable.setBounds(0, 0, 600, 200);
+        cursadasTable.setBounds(100, 100, 500, 200);
         return cursadasTable;
     }
 
@@ -66,6 +67,43 @@ public class FrontEnd extends JFrame implements ActionListener {
             label1.setText("Aprovação no ultimo periodo: " + porcentagem + "%");
         }
         return label1;
+    }
+
+    private JTable faltantes() {
+        JTable faltantesTable = new JTable(modelFaltantes);
+        modelFaltantes.addColumn("codigo");
+        modelFaltantes.addColumn("disciplina");
+
+        // pega barreira
+        List<String> barreira = new ArrayList<>();
+        for (Ofertada ofertada : listaOfertadas.lista) {
+            if (ofertada.getperiodoIdeal() != 0 && ofertada.getperiodoIdeal() < 4)
+                barreira.add(ofertada.getCodDisciplina());
+        }
+
+        // pega aprovadas
+        List<String> aprovadas = new ArrayList<>();
+        for (Cursada cursada : listaCursadas.lista) {
+            if (cursada.getSituacao() == 1)
+                aprovadas.add(cursada.getCodDisciplina());
+        }
+
+        //pega faltantes
+        List<String> faltantes = new ArrayList<>();
+        for (String b : barreira)
+            if (!aprovadas.contains(b))
+                faltantes.add(b);
+
+        for (Ofertada ofertada : listaOfertadas.lista) {
+            if (faltantes.contains(ofertada.getCodDisciplina())) {
+                modelFaltantes.addRow(
+                        new Object[] { ofertada.getCodDisciplina(),
+                                ofertada.getNomeDisciplina() });
+            }
+        }
+
+        faltantesTable.setBounds(100, 100, 600, 200);
+        return faltantesTable;
     }
 
     public void actionPerformed(ActionEvent e) {
