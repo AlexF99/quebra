11069f5ba619ce8ba206788832da67a5a78d2a26
phks20 <phks20@inf.ufpr.br>
2022-09-14 20:45:41 -0300

    fix select & csv, add button, add table sorting

--- src/main/java/quebra/FrontEnd.java
+++ src/main/java/quebra/FrontEnd.java
@@ -16,57 +16,107 @@ public class FrontEnd extends JFrame implements ActionListener {
     private DefaultTableModel modelFaltantes = new DefaultTableModel();
     private DefaultTableModel modelOfertadas = new DefaultTableModel();
     private DefaultTableModel modelSelecionadas = new DefaultTableModel();
-
+    private String desempenho = "";
     ListaCursadas listaCursadas = ListaCursadas.getInstance();
     ListaOfertadas listaOfertadas = ListaOfertadas.getInstance();
 
     public FrontEnd() {
-        JTable cursadasView = cursadas();
+        JTable cursadas = cursadas();
         JLabel aprovacao = extraInfo();
         JTable faltantes = faltantes();
-        JTable selecionadas = selecionadas();
+        final JTable selecionadas = selecionadas();
         final JTable ofertadas = ofertadas();
-        
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(new JScrollPane(cursadasView));
-        panel.add(new JScrollPane(aprovacao));
-        panel.add(new JScrollPane(faltantes));
-        panel.add(new JScrollPane(ofertadas));
-        panel.add(new JScrollPane(selecionadas));
 
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, 1));
+        JButton button = new JButton("Enviar");
+        button.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        JScrollPane aprovacaoPane = new JScrollPane(aprovacao);
+        aprovacaoPane.setBorder(BorderFactory.createTitledBorder("Aprovação"));
+        JScrollPane cursadasPane = new JScrollPane(cursadas);
+        cursadasPane.setBorder(BorderFactory.createTitledBorder("Cursadas"));
+        JScrollPane ofertadasPane = new JScrollPane(ofertadas);
+        ofertadasPane.setBorder(BorderFactory.createTitledBorder("Ofertadas"));
+        JScrollPane faltantesPane = new JScrollPane(faltantes);
+        faltantesPane.setBorder(BorderFactory.createTitledBorder("Faltantes"));
+        JScrollPane selecionadasPane = new JScrollPane(selecionadas);
+        selecionadasPane.setBorder(BorderFactory.createTitledBorder("Selecionadas"));
+        panel.add(aprovacaoPane);
+        panel.add(cursadasPane);
+        panel.add(faltantesPane);
+        panel.add(ofertadasPane);
+        panel.add(selecionadasPane);
+        panel.add(button);
         this.add(panel, BorderLayout.CENTER);
+    
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("pedido de quebra");
         this.pack();
         this.setVisible(true);
 
-        final ListSelectionModel selectionModel = ofertadas.getSelectionModel();
-        selectionModel.addListSelectionListener(new ListSelectionListener() {
-            int cliclCount = 0;
+        final ListSelectionModel ofertadasSelectionModel = ofertadas.getSelectionModel();
+        ofertadasSelectionModel.addListSelectionListener(new ListSelectionListener() {
+            int clickCount = 0;
+            boolean exists = false;
+
             public void valueChanged(ListSelectionEvent event) {
-                if(!selectionModel.isSelectionEmpty() && cliclCount % 2 == 0){
-                    modelSelecionadas.addRow(
-                        new Object[] { ofertadas.getValueAt(ofertadas.getSelectedRow(), 0).toString(),
-                        });
+                // verifica se já existe na tabela
+                for (int i = 0; i < modelSelecionadas.getDataVector().size(); i++) {
+                    if (ofertadas.getValueAt(ofertadas.getSelectedRow(), 0).toString() == modelSelecionadas
+                            .getDataVector().get(i).get(0)) {
+                        exists = true;
                     }
-                cliclCount++;
+                }
+                // adiciona o elemento clicado só se não existir na tabela e o clickCount for
+                // par (para corrigir o bug da tabela que clica duas vezes)
+                if (!ofertadasSelectionModel.isSelectionEmpty() && clickCount % 2 == 0 && !exists) {
+                    modelSelecionadas.addRow(
+                            new Object[] { ofertadas.getValueAt(ofertadas.getSelectedRow(), 0).toString(),
+                            });
+                }
+                exists = false;
+                clickCount++;
             }
         });
-        
+
+        final ListSelectionModel selecionadasSelectionModel = selecionadas.getSelectionModel();
+        selecionadasSelectionModel.addListSelectionListener(new ListSelectionListener() {
+            int clickCount = 0;
+
+            public void valueChanged(ListSelectionEvent event) {
+                // remove o elemento clicado da tabela quando o clickCount for
+                // par (para corrigir o bug da tabela que clica duas vezes)
+                if (!selecionadasSelectionModel.isSelectionEmpty() && clickCount % 2 == 0) {
+                    modelSelecionadas.removeRow(selecionadas.getSelectedRow());
+                }
+                clickCount++;
+            }
+        });
+
         this.setSize(700, 800);
     }
 
     private JTable selecionadas() {
-        JTable selecionadasTable = new JTable(modelSelecionadas);
+        JTable selecionadasTable = new JTable(modelSelecionadas) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        // habilita o sort ao clicar no header
+        selecionadasTable.setAutoCreateRowSorter(true);
         modelSelecionadas.addColumn("codigo");
         selecionadasTable.setBounds(100, 100, 500, 500);
         return selecionadasTable;
     }
 
     private JTable cursadas() {
-        JTable cursadasTable = new JTable(modelCursadas);
+        JTable cursadasTable = new JTable(modelCursadas) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        // habilita o sort ao clicar no header
+        cursadasTable.setAutoCreateRowSorter(true);
         modelCursadas.addColumn("codigo");
         modelCursadas.addColumn("disciplina");
         modelCursadas.addColumn("periodo");
@@ -89,20 +139,28 @@ public class FrontEnd extends JFrame implements ActionListener {
     }
 
     private JTable ofertadas() {
-        JTable ofertadasTable = new JTable(modelOfertadas);
+        JTable ofertadasTable = new JTable(modelOfertadas) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         modelOfertadas.addColumn("codigo");
         modelOfertadas.addColumn("nome");
         modelOfertadas.addColumn("periodo ideal");
-        Boolean flag;
-
+        // habilita o sort ao clicar no header
+        ofertadasTable.setAutoCreateRowSorter(true);
+        Boolean exists;
+        // percorre a lista de ofertadas e de cursadas e adiciona na tabela de ofertadas
+        // apenas as que forem acima do terceiro período e que não foram cursadas
         for (Ofertada ofertada : listaOfertadas.lista) {
-            flag = true;
+            exists = false;
             for (Cursada cursada : listaCursadas.lista) {
-                if (ofertada.getCodDisciplina().equals(cursada.getCodDisciplina())) {
-                    flag = false;
+                if (ofertada.getCodDisciplina().equals(cursada.getCodDisciplina())
+                        && cursada.getStrSituacao().equals("aprovado")) {
+                    exists = true;
                 }
             }
-            if (flag) {
+            if (!exists && ofertada.getperiodoIdeal() > 3) {
                 modelOfertadas.addRow(
                         new Object[] { ofertada.getCodDisciplina(),
                                 ofertada.getNomeDisciplina(),
@@ -119,9 +177,15 @@ public class FrontEnd extends JFrame implements ActionListener {
         JLabel label1 = new JLabel("aprovacao");
         double ultimasAprovadas = 0;
         double ultimasCursadas = 0;
+        double ira = 0;
+        double soma = 0;
+        double chTotal = 0;
         int reprovacoesFalta = 0;
 
         for (Cursada cursada : listaCursadas.lista) {
+            soma = soma + cursada.getMedia() * cursada.getCargaHoraria();
+            chTotal += cursada.getCargaHoraria();
+
             if (cursada.getPeriodo() == 3) {
                 ultimasCursadas++;
                 if (cursada.getSituacao() == 1)
@@ -131,18 +195,35 @@ public class FrontEnd extends JFrame implements ActionListener {
             if (cursada.getSituacao() == 3)
                 reprovacoesFalta++;
         }
-        String porcentagem = (String) String.format("%.2f", ((ultimasAprovadas / ultimasCursadas) * 100));
+        ira = soma / (chTotal * 100);
+        double fracao = ultimasAprovadas / ultimasCursadas;
+        String porcentagem = (String) String.format("%.2f", ((fracao) * 100));
+
+        if (fracao > (2.0 / 3.0))
+            desempenho = "Bom";
+        else if (fracao <= 2.0 / 3.0 && fracao > 1.0 / 2.0)
+            desempenho = "Médio";
+        else if (fracao < 1.0 / 2.0)
+            desempenho = "Ruim";
+
         if (ultimasCursadas != 0) {
-            label1.setText("Aprovação no ultimo periodo: " + porcentagem + "%" + " Reprovações por falta: "
-                    + reprovacoesFalta);
+            label1.setText("Aprovação no ultimo periodo: " + porcentagem + "% " + " \nReprovações por falta: "
+                    + reprovacoesFalta + " \nIRA: " + ira + " Desempenho: " + desempenho);
         }
         return label1;
     }
 
     private JTable faltantes() {
-        JTable faltantesTable = new JTable(modelFaltantes);
+        JTable faltantesTable = new JTable(modelFaltantes) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        // habilita o sort ao clicar no header
+        faltantesTable.setAutoCreateRowSorter(true);
         modelFaltantes.addColumn("codigo");
         modelFaltantes.addColumn("disciplina");
+        modelFaltantes.addColumn("periodo");
 
         // pega barreira
         List<String> barreira = new ArrayList<>();
@@ -168,7 +249,7 @@ public class FrontEnd extends JFrame implements ActionListener {
             if (faltantes.contains(ofertada.getCodDisciplina())) {
                 modelFaltantes.addRow(
                         new Object[] { ofertada.getCodDisciplina(),
-                                ofertada.getNomeDisciplina() });
+                                ofertada.getNomeDisciplina(), ofertada.getperiodoIdeal() });
             }
         }
 
